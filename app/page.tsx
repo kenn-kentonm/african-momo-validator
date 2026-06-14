271b6fb7374584f4be8f44f027186d9c31320a82
kenn-kentonm <sales@predictiqpro.com>
2026-06-14 13:09:46 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>African Mobile Money Validator API</h1>
+      <p>See documentation on RapidAPI.</p>
+    </main>
+  )
+}
